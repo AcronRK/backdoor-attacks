3a86be474ec272a6f5bdd38b2faa3192b7e78d2c
Reni <r.koci98@gmail.com>
2024-02-11 20:00:22 +0100

    added unit tests and dataset poisoning

--- unit_tests/tests_poison.py
+++ unit_tests/tests_poison.py
@@ -1,21 +1,45 @@
 import torch
-import torchvision.transforms.functional as F
+import torchvision.transforms as transforms
+from torchvision.datasets import MNIST
 import matplotlib.pyplot as plt
 import unittest
+import random
 
 import sys
 # import poison file
-sys.path.append('../utils')
+sys.path.append('../')
 from utils import poison
 
 class PoisonTest(unittest.TestCase):
+    
     def setUp(self):
         self.poison = poison.Poison()
         
-    def test_add_patch_to_corner(self, patch_size=2, loc="top-left"):
+    def test_img(self, patch_size=2, loc="top-left"):
+        # Load the MNIST dataset
+        transform = transforms.Compose([transforms.ToTensor()])
+        mnist_dataset = MNIST(root='../data', train=True, transform=transform, download=True)
         
-        # create a sample image
-        image = torch.zeros(1, 1, 8, 8)  # Batch size of 1, 1 channel, 8x8 image
+        image1 = mnist_dataset[17460][0]
+        image2 = mnist_dataset.data[17460]
+        image3 = self.poison.add_patch_to_corner(image1, patch_size=patch_size, loc=loc)
+        mnist_dataset.data[17460] = self.poison.add_patch_to_corner(image1, patch_size=patch_size, loc=loc)
+        # Plot the original and poisoned images
+        fig, axs = plt.subplots(1, 3)
+        axs[0].imshow(image1.squeeze(), cmap='gray')
+        axs[0].set_title('Original Image')
+        axs[1].imshow(image2.squeeze(), cmap='gray')
+        axs[1].set_title('Poisoned Image')
+        axs[2].imshow(image3.squeeze(), cmap='gray')
+        axs[2].set_title('Poisoned Image')
+        plt.show()   
+        
+    def test_add_patch_to_corner(self, patch_size=2, loc="top-left"):
+        # Load the MNIST dataset
+        transform = transforms.Compose([transforms.ToTensor()])
+        mnist_dataset = MNIST(root='../data', train=True, transform=transform, download=True)
+        
+        image = mnist_dataset[46070][0]
         
         poisoned_image = self.poison.add_patch_to_corner(image, patch_size=patch_size, loc=loc)
         
@@ -25,7 +49,34 @@ class PoisonTest(unittest.TestCase):
         # Plot the original and poisoned images
         fig, axs = plt.subplots(1, 2)
         axs[0].imshow(image.squeeze(), cmap='gray')
-        axs[0].set_title('Original Image')
+        axs[0].set_title(f'Original Image')
         axs[1].imshow(poisoned_image.squeeze(), cmap='gray')
         axs[1].set_title('Poisoned Image')
         plt.show()
+        
+    def test_poison_dataset_patch_to_corner(self):
+        # Load the MNIST dataset
+        transform = transforms.Compose([transforms.ToTensor()])
+        mnist_dataset = MNIST(root='../data', train=True, transform=transform, download=True)
+
+        # Select original and target labels
+        original_label = 3
+        target_label = 8
+        # Poison a portion of the dataset
+        poisoned_dataset, poisoned_indices = self.poison.poison_dataset_patch_to_corner(mnist_dataset, original_label, target_label, poison_ratio=0.1)
+        
+        # assert the number of samples
+        self.assertEqual(len(mnist_dataset), len(poisoned_dataset))
+
+        # Plot some poisoned images for inspection
+        fig, axes = plt.subplots(1, 5, figsize=(12, 3))
+        for i in range(5):
+            idx = random.choice(poisoned_indices)
+            print(idx)
+            image, label = poisoned_dataset[idx]
+            axes[i].imshow(image.squeeze(), cmap='gray')
+            axes[i].set_title(f'Label: {label}')
+            axes[i].axis('off')
+        plt.show()
+        
+        return poisoned_dataset
--- utils/poison.py
+++ utils/poison.py
@@ -1,24 +1,61 @@
 import torch
-import torchvision.transforms.functional as F
+import random
+import copy
 
 class Poison:
     def __init__(self) -> None:
         pass
+    
+    def poison_dataset_patch_to_corner(self, dataset, original_label, target_label, poison_ratio=0.1, patch_size=2, patch_value=1.0, loc="top-left"): 
+        """
+        Poison a portion of the dataset with the original label assigned to the target label.
+
+        Args:
+        - dataset (torch.utils.data.Dataset): The input dataset.
+        - original_label (int): The original label to be poisoned.
+        - target_label (int): The target label to be assigned to the poisoned samples.
+        - poison_method (callable): A poisoning method that takes an image tensor and returns a poisoned image tensor.
+        - poison_ratio (float): The ratio of the dataset to be poisoned.
+
+        Returns:
+        - torch.utils.data.Dataset: The poisoned dataset.
+        - list: The poisoned indices.
+        """
+        # Get all indices corresponding to the original label
+        original_label_indices = torch.arange(len(dataset))[dataset.targets == original_label]
+        # Get the number of samples to poison (e.g., if poison_ratio is 0.1 -> 10% of samples will be poisoned)
+        num_poisoned_samples = int(len(original_label_indices) * poison_ratio)
+
+        # Select random indices for the original label
+        selected_indices = random.sample(original_label_indices.tolist(), num_poisoned_samples)
+
+        poisoned_dataset = []
+
+        # Apply the poisoning method to the selected subset
+        for idx, (image, label) in enumerate(dataset):
+            if idx in selected_indices:
+                # Poison the image and assign the target label
+                image = self.add_patch_to_corner(image, patch_size=patch_size, patch_value=patch_value, loc=loc)
+                label = target_label
+            poisoned_dataset.append((image, label))
+
+        return poisoned_dataset, selected_indices
+            
     
     def add_patch_to_corner(self, image, patch_size=2, patch_value=1.0, loc="top-left"):
         # clone the image
         poisoned_image = image.clone()
         
         # check which location is specified
-        # image dimensions -> (batch_size, channels, height, width)
+        # image dimensions -> (channels, height, width)
         if loc == "top-left":
-            poisoned_image[:, :, :patch_size, :patch_size] = patch_value
+            poisoned_image[ :, :patch_size, :patch_size] = patch_value
         elif loc == "top-right":
-            poisoned_image[:, :, :patch_size, -patch_size:] = patch_value
+            poisoned_image[ :, :patch_size, -patch_size:] = patch_value
         elif loc == "bottom-left":
-            poisoned_image[:, :, -patch_size:, :patch_size] = patch_value
+            poisoned_image[ :, -patch_size:, :patch_size] = patch_value
         elif loc == "bottom-right":
-            poisoned_image[:, :, -patch_size:, -patch_size:] = patch_value
+            poisoned_image[ :, -patch_size:, -patch_size:] = patch_value
         else:
             print("Unknown location value")
             return
